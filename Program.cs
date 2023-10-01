2c4e7791b8cc8916ba4b9f2885d278764fe9cc40
Umair <umair.abid96@gmail.com>
2023-10-01 16:51:03 +0400

    feat: remove data file

--- Program.cs
+++ Program.cs
@@ -33,6 +33,7 @@ namespace gRPC_dotNET7
 
             // Configure the HTTP request pipeline.
             app.MapGrpcService<GreeterService>();
+            app.MapGrpcService<SalesDataServices>();
             app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
             app.Run();
---
+++ gRPC_dotNET_Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using gRPC_dotNET_Client.Protos;
+
+namespace gRPC_dotNET_Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, gRPC World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:7081");
+            int Count = 0;
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            try
+            {
+                var client = new SalesService.SalesServiceClient(channel);
+
+                using var call = client.GetSalesData(new Request { Filters = "" }
+                  , deadline: DateTime.UtcNow.AddMinutes(10)
+                );
+
+                await foreach (var each in call.ResponseStream.ReadAllAsync())
+                {
+
+                    Console.WriteLine(String.Format("New Order Receieved from {0}-{1},Order ID = {2}, Unit Price ={3}, Ship Date={4}", each.Country, each.Region, each.OrderID, each.UnitPrice, each.ShipDate));
+                    Count++;
+
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.DeadlineExceeded)
+            {
+                Console.WriteLine("Service timeout.");
+            }
+
+            watch.Stop();
+
+            Console.WriteLine($"Stream ended: Total Records:{Count.ToString()} in {watch.Elapsed.TotalMinutes} minutes and {watch.Elapsed.TotalSeconds} seconds.");
+            Console.Read();
+        }
+    }
+}
